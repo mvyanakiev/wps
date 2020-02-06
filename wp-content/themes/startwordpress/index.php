a0cf3732a07ceb190d836e16af0e61d0e33dde17
mvyanakiev <myanakiev@gmail.com>
2020-02-06 18:33:24 +0200

    comments added

--- wp-content/themes/startwordpress/index.php
+++ wp-content/themes/startwordpress/index.php
@@ -3,9 +3,20 @@
 		<div class="col-sm-8 blog-main">
 
 			<?php
-			if ( have_posts() ) : while ( have_posts() ) : the_post();
-				get_template_part( 'content', get_post_format() );
-			endwhile; endif;
+				if ( have_posts() ) : while ( have_posts() ) : the_post();
+					get_template_part( 'content', get_post_format() );
+				endwhile; 
+			?>
+			
+			<nav>
+				<ul class="pager">
+					<li><?php next_posts_link( 'Previous' ); ?></li>
+					<li><?php previous_posts_link( 'Next' ); ?></li>
+				</ul>
+			</nav>
+						
+			<?php
+				endif;
 			?>
 
 		</div> <!-- /.blog-main -->
