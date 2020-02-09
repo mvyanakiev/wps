b0b7da6fb9fe2a0d5c9961451eae84db9157ec12
Milko Yanakiev <myanakiev@hotmail.com>
2020-02-09 17:33:57 +0200

    sidebar fix

--- wp-content/themes/startwordpress/sidebar.php
+++ wp-content/themes/startwordpress/sidebar.php
@@ -12,9 +12,9 @@
           <div class="sidebar-module">
             <h4>Elsewhere</h4>
             <ol class="list-unstyled">
-                <li><a href="<?php echo get_option('github'); ?>">GitHub</a></li>
-                <li><a href="<?php echo get_option('twitter'); ?>">Twitter</a></li>
-                <li><a href="<?php echo get_option('facebook'); ?>">Facebook</a></li>
+                <li><a href="<?php echo get_option('github'); ?> " target="_blank">GitHub</a></li>
+                <li><a href="<?php echo get_option('twitter'); ?> " target="_blank">Twitter</a></li>
+                <li><a href="<?php echo get_option('facebook'); ?>" target="_blank">Facebook</a></li>
             </ol>
           </div>
         </div><!-- /.blog-sidebar -->
